e2b77913caac04bc708091fb0045f0e0d9259f76
ReveredRedHood <reveredredhood@pm.me>
2024-10-13 01:29:40 -0600

    remove deps from repo, and abandoned mods

--- mods/TestHelper/Plugin.cs
+++ mods/TestHelper/Plugin.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections;
 using System.Linq;
 using BepInEx;
-using BepInEx.Configuration;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 using SOD.Common;
@@ -20,14 +18,137 @@ namespace TestHelper {
 
         public override void Load() {
             // Plugin startup logic
-            LogUtils.Load(Log);
             Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
 
             // Harmony.PatchAll();
             // Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is patched!");
 
-            Lib.InputDetection.OnButtonStateChanged += OnButton;
+            // Lib.InputDetection.OnButtonStateChanged += OnButtonStateChanged;
+            // Lib.InputDetection.OnAxisStateChanged += OnAxisStateChanged;
+            // Lib.InputDetection.OnSuppressedButtonStateChanged += OnSuppressedButtonStateChanged;
+            // Lib.InputDetection.OnSuppressedAxisStateChanged += OnSuppressedAxisStateChanged;
+            Lib.SaveGame.OnAfterLoad += Test;
+
             UniverseLib.RuntimeHelper.StartCoroutine(SkipLoadCoroutine());
+        }
+
+        // private void OnSuppressedAxisStateChanged(object sender, SuppressedAxisInputDetectionEventArgs e) {
+        //     if (e.Key != InteractablePreset.InteractionKey.moveVertical && e.Key != InteractablePreset.InteractionKey.moveHorizontal) {
+        //         return;
+        //     }
+        //     Log.LogInfo($"SuppressedAxisStateChanged {e.ActionName} {e.AxisValue}");
+        // }
+
+        // private void OnSuppressedButtonStateChanged(object sender, SuppressedInputDetectionEventArgs e) {
+        //     if (e.Key != InteractablePreset.InteractionKey.primary && e.Key != InteractablePreset.InteractionKey.secondary) {
+        //         return;
+        //     }
+        //     Log.LogInfo($"SuppressedButtonStateChanged {e.ActionName}");
+        // }
+
+        // private void OnAxisStateChanged(object sender, AxisInputDetectionEventArgs e) {
+        //     if (e.Key != InteractablePreset.InteractionKey.moveVertical && e.Key != InteractablePreset.InteractionKey.moveHorizontal) {
+        //         return;
+        //     }
+        //     Log.LogInfo($"AxisStateChanged {e.ActionName} {e.AxisValue}");
+        // }
+
+        // private void OnButtonStateChanged(object sender, InputDetectionEventArgs e) {
+        //     if (e.Key != InteractablePreset.InteractionKey.primary && e.Key != InteractablePreset.InteractionKey.secondary) {
+        //         return;
+        //     }
+        //     Log.LogInfo($"ButtonStateChanged {e.ActionName}");
+        // }
+
+        private void Test(object sender, SaveGameArgs e) {
+            UniverseLib.RuntimeHelper.StartCoroutine(TestCoroutine());
+        }
+
+        private IEnumerator TestCoroutine() {
+            yield return new WaitForEndOfFrame();
+            // var guid = MyPluginInfo.PLUGIN_GUID;
+
+            // Log.LogInfo(Lib.InputDetection.RewiredPlayer.descriptiveName);
+
+            // Log.LogInfo(Lib.InputDetection.GetBinding(InteractablePreset.InteractionKey.primary));
+            // Log.LogInfo(Lib.InputDetection.GetRewiredAction(InteractablePreset.InteractionKey.crouch).name);
+            // Log.LogInfo(Lib.InputDetection.GetRewiredActionName(InteractablePreset.InteractionKey.crouch));
+
+            // // Suppressed keycode and suppressed action
+            // Lib.InputDetection.SetInputSuppressed(guid, KeyCode.Mouse2);
+            // Lib.InputDetection.SetInputSuppressed(guid, InteractablePreset.InteractionKey.flashlight);
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, KeyCode.Mouse2, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, InteractablePreset.InteractionKey.flashlight, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, InteractablePreset.InteractionKey.flashlight, out _));
+            // // Unsuppressed keycode, but suppressed action
+            // Lib.InputDetection.SetInputSuppressed(guid, InteractablePreset.InteractionKey.LeanLeft);
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, KeyCode.Q, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, InteractablePreset.InteractionKey.LeanLeft, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, InteractablePreset.InteractionKey.LeanLeft, out _));
+            // // Suppressed keycode, indirectly suppressed action
+            // Lib.InputDetection.SetInputSuppressed(guid, KeyCode.E);
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, KeyCode.E, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, InteractablePreset.InteractionKey.LeanRight, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, InteractablePreset.InteractionKey.LeanRight, out _));
+            // // Remove suppression
+            // Lib.InputDetection.RemoveInputSuppression(InteractablePreset.InteractionKey.flashlight);
+            // Lib.InputDetection.RemoveInputSuppression(KeyCode.E);
+            // // Check
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, KeyCode.Mouse2, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, InteractablePreset.InteractionKey.flashlight, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, InteractablePreset.InteractionKey.flashlight, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, KeyCode.Q, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, InteractablePreset.InteractionKey.LeanLeft, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, InteractablePreset.InteractionKey.LeanLeft, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, KeyCode.E, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, InteractablePreset.InteractionKey.LeanRight, out _));
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, InteractablePreset.InteractionKey.LeanRight, out _));
+
+            // Lib.InputDetection.RemoveInputSuppression(InteractablePreset.InteractionKey.flashlight);
+            // Log.LogInfo(Lib.InputDetection.IsInputSuppressed(guid, KeyCode.Mouse2, out _));
+
+            // Log.LogInfo(Lib.InputDetection.GetBinding(InteractablePreset.InteractionKey.jump));
+
+            // Log.LogInfo("Suppressed sprint for 5 sec");
+            // Lib.InputDetection.SetInputSuppressed(guid, InteractablePreset.InteractionKey.sprint, TimeSpan.FromSeconds(5.0f), true);
+            // Log.LogInfo("Suppressed LeanLeft for 5 sec");
+            // Lib.InputDetection.SetInputSuppressed(guid, InteractablePreset.InteractionKey.LeanLeft, TimeSpan.FromSeconds(5.0f), true);
+            // Log.LogInfo("Suppressed caseBoard for 10 sec");
+            // Lib.InputDetection.SetInputSuppressed(guid, InteractablePreset.InteractionKey.caseBoard, TimeSpan.FromSeconds(10.0f), true);
+            // Log.LogInfo("Suppressed moveHorizontal for 5 sec");
+            // Lib.InputDetection.SetInputSuppressed(guid, InteractablePreset.InteractionKey.moveHorizontal, TimeSpan.FromSeconds(15.0f), true);
+            // Log.LogInfo("Just kidding, suppressed moveHorizontal for 15 sec");
+            // Lib.InputDetection.SetInputSuppressed(guid, InteractablePreset.InteractionKey.moveHorizontal, TimeSpan.FromSeconds(15.0f), true);
+            // Log.LogInfo("Unsuccessfully suppressing moveHorizontal for 60 sec");
+            // Lib.InputDetection.SetInputSuppressed(guid, InteractablePreset.InteractionKey.moveHorizontal, TimeSpan.FromSeconds(60.0f), false);
+            // Log.LogInfo("Suppressed flashlight indefinitely");
+            // Lib.InputDetection.SetInputSuppressed(guid, InteractablePreset.InteractionKey.flashlight);
+            // Log.LogInfo("Do I see the LeanRight suppression entry under a different guid?");
+            // yield return new WaitForSeconds(1.0f);
+            // if (!Lib.InputDetection.IsInputSuppressedByAnyPlugin(InteractablePreset.InteractionKey.LeanRight, out var suppressedBy, out _)) {
+            //     Log.LogInfo("I don't see any suppression entry (expected on first run prior to save)");
+            //     // Log.LogInfo($"Troubleshoot: {Lib.InputDetection.FindInputSuppressionEntries(_ => true).ToStringEnumerable()}");
+            // }
+            // else {
+            //     Log.LogInfo("I see these entries for LeanRight");
+            //     Log.LogInfo(suppressedBy.First());
+            //     Log.LogInfo(suppressedBy.Last());
+            // }
+            // if (!Lib.InputDetection.IsInputSuppressed(guid, InteractablePreset.InteractionKey.LeanRight, out _)) {
+            //     Log.LogInfo("I don't see any suppression entry for the actual guid");
+            //     // Log.LogInfo($"Troubleshoot: {Lib.InputDetection.FindInputSuppressionEntries(_ => true).ToStringEnumerable()}");
+            // }
+            // else {
+            //     Log.LogInfo("LeanRight is suppressed by this plugin's actual guid");
+            // }
+            // yield return new WaitForSeconds(5.0f);
+            // Log.LogInfo("Suppressed LeanRight under this guid");
+            // Lib.InputDetection.SetInputSuppressed(guid, InteractablePreset.InteractionKey.LeanRight);
+            // Log.LogInfo("Suppressed LeanRight indefinitely under a different guid");
+            // Lib.InputDetection.SetInputSuppressed("helloworld", InteractablePreset.InteractionKey.LeanRight);
+            // // suppress moveHorizontal, secondary
+            // Lib.InputDetection.SetInputSuppressed(guid, InteractablePreset.InteractionKey.moveHorizontal);
+            // Lib.InputDetection.SetInputSuppressed(guid, InteractablePreset.InteractionKey.secondary);
         }
 
         // Skip the intro and just load into a game
@@ -65,19 +186,9 @@ namespace TestHelper {
             MainMenuController.Instance.SetMenuComponent(MainMenuController.Component.loadingCity);
         }
 
-        internal static void AddExceptMsg(System.Action action, string addMsgOnException) {
-            try {
-                action();
-            }
-            catch (System.Exception except) {
-                throw new System.InvalidOperationException(innerException: except, message: addMsgOnException);
-            }
-        }
-
         public override bool Unload() {
             // Harmony?.UnpatchSelf();
 
-            LogUtils.Unload();
             return base.Unload();
         }
     }
